a85dfb106220b58dd79063c6b535212cb5eb40de
Esraamabrouk2022 <esraamabrouk126@gmail.com>
2023-10-24 01:15:01 +0300

    Commit security

---
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DemoSecurityConfig {
+
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id,role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterchain(HttpSecurity http) throws  Exception{
+    http.authorizeHttpRequests(configurer->
+            configurer
+                    .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+
+    );
+    http.httpBasic(Customizer.withDefaults());
+    http.csrf(csrf->csrf.disable());
+    return http.build();
+
+    }
+
+    /*
+    *   @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john= User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary=User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+        UserDetails suson=User.builder()
+                .username("suson")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(john,mary,suson);
+    }
+*/
+
+
+}
